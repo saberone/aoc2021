f2acc2bf5ef9419fcccac6933eb2714be9fa81e4
Saber Karmous <saberone@gmail.com>
2021-12-01 15:52:38 +0100

    Initial commit

---
+++ Day1/Puzzle1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int? previousValue = null;
+var increasedCount = 0;
+
+foreach (string line in File.ReadLines(@"input.txt"))
+{
+    int currentValue = int.Parse(line.Trim());
+    Console.Write($"{currentValue}: ");
+    if (currentValue > previousValue)
+    {
+        Console.WriteLine($"increased");
+        increasedCount++;
+    } else if (currentValue < previousValue)
+    {
+        Console.WriteLine($"decreased");
+    }
+    previousValue = currentValue;
+}
+Console.WriteLine($"Measurements larger than previous {increasedCount}");
+Console.ReadKey();
